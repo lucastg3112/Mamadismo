9e141ea8a6a600ba01975ea2bf097a55e83f4352
lucastg3112 <57245190+lucastg3112@users.noreply.github.com>
2019-11-01 13:39:04 -0500

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,19 @@
+var respuesta;
+
+respuesta = prompt("¿Esta usted mamadisimo?? SI/NO","");
+
+
+if (respuesta == "si" || respuesta == "SI") {
+
+	document.write("<h3>Estas mamadisimo como Lance!!</h3>");
+}
+else if (respuesta == "no" || respuesta=="NO") {
+
+	document.write("<h3>Deja de ser vago para estar mamadisimo como Lance !!!!</h3>");
+}else{
+
+	document.write("<h3>respuesta no permitida recargue la pagina</h3>");
+}
+
+
+
