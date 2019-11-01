77bb6c2dea86d2cededb538343d6eb56dc1313c7
lucastg3112 <57245190+lucastg3112@users.noreply.github.com>
2019-11-01 13:46:33 -0500

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -12,7 +12,7 @@ else if (respuesta == "no" || respuesta=="NO") {
 	document.write("<h3>Deja de ser vago para estar mamadisimo como Lance !!!!</h3>");
 }else{
 
-	document.write("<h3>respuesta no permitida recargue la pagina</h3>");
+	document.write("<h3>recargue la pagina </h3><br> <h1>respuesta no permitida</h1>");
 }
 
 
